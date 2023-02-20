4f84c307baf3bc5f103f0e80b7a927a9e1037b46
ltxuan <lethixuanx7@gmail.com>
2023-02-20 14:51:23 +0700

    pass view Engine

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
-//const express = require('express')
 import express from "express";
+import configViewEngine from "./configs/viewengine";
 const app = express()
 const port = 8080
-const path = require('path');
 
+configViewEngine(app)
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.render('index.ejs')
 })
 
 app.listen(port, () => {
