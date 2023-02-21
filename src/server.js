c6579ec7370bcd265dca73b2e88666601ab8b91c
ltxuan <lethixuanx7@gmail.com>
2023-02-21 08:26:48 +0700

    static file color

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import configViewEngine from "./configs/viewengine";
+require('dotenv').config()
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080;
+console.log('>>> check port: ', port)
 
 configViewEngine(app)
 app.get('/', (req, res) => {
